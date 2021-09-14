8fad46384ccad01401e4a81e3fd914b4ac38ae09
SalvadorSC <sanchezcampossalvador@gmail.com>
2021-09-14 16:20:01 +0200

    fix: totalClicks set to 0

--- src/context/CounterContext.js
+++ src/context/CounterContext.js
@@ -6,7 +6,7 @@ export const CounterProvider = ({ children }) => {
   const [multiplicador, setMultiplicador] = useState(0);
   const [automatron1, setAutomatron1] = useState(0);
   const [count, setCount] = useState(0);
-  const [totalCountOfAllTime, setTotalCountOfAllTime] = useState(48);
+  const [totalCountOfAllTime, setTotalCountOfAllTime] = useState(0);
   const [goal, setGoal] = useState(100);
   return (
     <CounterContext.Provider
